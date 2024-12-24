2205ba8ae388287b2ccf6b940298f9b7d2b5f139
pr0.G <youssefmo5904@gmail.com>
2024-12-24 08:40:36 +0200

    added readline

--- ps/index.js
+++ ps/index.js
@@ -1,6 +1,17 @@
-const inputArr = ["++", "++", "--", "++"];
+const readline = require("readline");
 
-console.log(inputArr.filter((val) => val === "++").length - inputArr.filter((val) => val === "--").length);
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
-// Get number of "++" elements, and "--" as well
-// Apply the calculation: ("++" count) - ("--" count)
+rl.question("", (input) => {
+	// Input example: ++ ++ -- ++
+	const inputArr = input.split(" ").map((item) => item.trim());
+
+	console.log(inputArr.filter((val) => val === "++").length - inputArr.filter((val) => val === "--").length);
+	// Get number of "++" elements, and "--" as well
+	// Apply the calculation: ("++" count) - ("--" count)
+
+	rl.close();
+});
